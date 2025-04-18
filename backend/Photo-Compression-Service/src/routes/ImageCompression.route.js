72a06cdd1e185a417c3ba971eee27c2b8926112c
kimosoft1 <kariiimashraf.1@gmail.com>
2025-04-19 01:38:55 +0200

    chore: remove old image compression service

--- backend/Photo-Compression-Service/src/routes/ImageCompression.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const multer = require('multer');
-const upload = multer({ storage: multer.memoryStorage() });
-const ImageCompressionController = require('../controllers/ImageCompression.controller');
-
-router.post('/compress', upload.single('file'), ImageCompressionController.compressImage);
-
-module.exports = router;
