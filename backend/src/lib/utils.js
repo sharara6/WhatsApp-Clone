214cdcea13c1562af75b0b1cb80dcf4d8438b274
Ahmed-Taha-981 <122402269+Ahmed-Taha-981@users.noreply.github.com>
2025-04-18 02:08:51 +0200

    feat: add login, logout and update profile

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,4 +1,6 @@
 import jwt from 'jsonwebtoken';
+
+
 export const generateToken = (userId,res) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
          expiresIn: '7d' 
@@ -6,7 +8,7 @@ export const generateToken = (userId,res) => {
         
     
     res.cookie('jwt', token, { 
-        maxAge: 7*24*60*60*1000, //  1 day in milliseconds
+        maxAge: 7*24*60*60*1000, //  7 day in milliseconds
         httpOnly: true, // The cookie is not accessible via JavaScript(this prevents XSS(cross site scripting) attacks)
         sameSite: 'strict', // The cookie is not sent along with cross-site requests (this prevents CSRF(cross site request forgery) attacks)
         secure: process.env.NODE_ENV !== 'development', // The cookie is only sent over HTTPS in production
