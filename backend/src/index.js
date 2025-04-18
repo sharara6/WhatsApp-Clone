0e54e353818df5338ae890b01cc62bf5a7f8b022
nour-awad <nour.awad094@gmail.com>
2025-04-18 19:21:12 +0200

    implemented socket.io to activate real-time chatting

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,20 +4,17 @@ import dotenv from 'dotenv';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import messageRoutes from './routes/message.route.js';
-
+import { app, server } from './lib/socket.js';
 
 dotenv.config();
-const app = express();
 const PORT=process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
-
-
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log('Server is running on port:', PORT);
     connectDB();
 });
